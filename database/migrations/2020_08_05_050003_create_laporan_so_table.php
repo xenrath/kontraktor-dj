cf1740f9850540cf7161991d55ce2dcaf71644f9
xenrath <xenrath89@gmail.com>
2020-08-05 12:01:19 +0700

    menambahkan migration

---
+++ database/migrations/2020_08_05_050003_create_laporan_so_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaporanSoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laporan_so', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laporan_so');
+    }
+}
--- resources/views/master-customer/index.blade.php
+++ resources/views/master-customer/index.blade.php
@@ -1,7 +1,45 @@
 @extends('layouts.main')
 @section('title', 'Master Customer')
 @section('content')
-<div class="block-header">
-    <h2>Master Customer</h2>
+<!-- #END# Basic Examples -->
+<!-- Exportable Table -->
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    EXPORTABLE TABLE
+                </h2>
+                <ul class="header-dropdown m-r--5">
+                    <li class="dropdown">
+                        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <i class="material-icons">more_vert</i>
+                        </a>
+                        <ul class="dropdown-menu pull-right">
+                            <li><a href="javascript:void(0);">Action</a></li>
+                            <li><a href="javascript:void(0);">Another action</a></li>
+                            <li><a href="javascript:void(0);">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-hover dataTable js-exportable">
+                        <thead>
+                        	<tr>
+                        		<th></th>
+                        	</tr>
+                        </thead>
+                    	<tbody>
+                    		<tr>
+                    			<td></td>
+                    		</tr>
+                    	</tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
